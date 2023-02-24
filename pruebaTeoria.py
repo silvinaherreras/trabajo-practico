b8d24f30441f4cc4901c2b3d9c8c735e2f58e2f0
silvinaherreras <121971765+silvinaherreras@users.noreply.github.com>
2023-02-24 10:32:37 -0300

    ejercicio teorio

--- pruebaTeoria.py
+++ pruebaTeoria.py
@@ -11,4 +11,37 @@ def lee_opcion():
             print(f'La tecla {opcion} no es una opción válida.')
     return opcion
 
-lee_opcion()
+#lee_opcion()
+def Problema81 ():
+    """
+    Queremos escribir un programa que nos permita armar la lista de los inscriptos
+    de una materia.
+    Mantenimiento: No permitir que haya legajos repetidos.
+    Mantenimiento: Permitir guardar el nombre y apellido de los estudiantes."""
+    print("Inscripciones al curso de Introducción a la Programación")
+    inscriptos=[]
+    datos_personales=[]
+    centinela=input("ingrese legajo para terminar ingrese *")
+    while centinela !="*":
+        if centinela in inscriptos:
+            print(f"el legajo {centinela} ya esta inscripto")
+        else:
+            inscriptos.append(centinela)
+            nombre_apellido=input("Ingrese el nombre y apellido: ")
+            datos_personales.append(nombre_apellido)
+        centinela=input("ingrese legajo para terminar ingrese *")
+    return inscriptos,datos_personales
+  
+
+def imprimirInscriptos(inscriptos,datos_personales):
+    for posicion,legajo in enumerate( inscriptos):
+        print(posicion,legajo,datos_personales[posicion])
+
+def main():
+    legajos,datos_personales=Problema81()
+    print("La lista de inscriptos es:",legajos)
+    imprimirInscriptos(legajos,datos_personales)
+    for i in range(len(legajos)):
+        print(f"Inscripto {i}: ({legajos[i]}) {datos_personales[i]}")
+
+main()
